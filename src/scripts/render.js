bb8fd34307dbbbadc1282abd5679a39c9cd78288
agkar <a.gkaravelis@gmail.com>
2025-07-18 13:00:59 +0300

    update renderer

---
+++ src/scripts/render.js
@@ -0,0 +1,78 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+const sharp = require('sharp');
+const ModelMap = require('../data/model-index.SampleAssets.json'); // Adjust the path to your JSON file
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: "new", // use "new" headless mode
+    args: ['--disable-web-security', '--allow-file-access-from-files', '--enable-webgl', '--ignore-gpu-blacklist', '--use-gl=angle', '--no-sandbox', '--disable-setuid-sandbox'],
+  });
+
+  const page = await browser.newPage();
+  page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+  page.on('pageerror', err => console.error('PAGE ERROR:', err));
+  page.on('requestfailed', request => {
+    console.error(`❌ FAILED REQUEST: ${request.url()} - ${request.failure().errorText}`);
+  });
+
+  // Dummy load to avoid stalls later in the process
+  const htmlPath = `file://${path.resolve(__dirname, 'viewer.html')}?model=DamagedHelmet.glb`;  
+  await page.goto(htmlPath, { waitUntil: 'networkidle2' });
+  // Set viewport and take screenshot
+  await page.setViewport({ width: 1920, height: 1080 });
+  await new Promise(resolve => setTimeout(resolve, 3000));  
+
+  // Create thumbnails dir
+  //const imagesDir = path.resolve(__dirname, "images");
+  const imagesDir = path.resolve(__dirname, "..", "..", "public", "images2");
+  fs.mkdirSync(imagesDir, { recursive: true });
+
+  const model_names = Object.keys(ModelMap)//.filter((e,i) => i == 2);
+  for(const model_name of model_names)
+  {
+    const model = ModelMap[model_name].gltfModel;
+    console.log("Processing", model_name, model);
+    const yaw = ModelMap[model_name].camera.yaw;
+    const pitch = ModelMap[model_name].camera.pitch;
+    const distance = ModelMap[model_name].camera.distance;
+    const htmlPath = `file://${path.resolve(__dirname, 'viewer.html')}?model=${model}&yaw=${yaw}&pitch=${pitch}&distance=${distance}`;
+    
+    await page.goto(htmlPath, { waitUntil: 'networkidle2' });
+    await page.waitForNetworkIdle();
+
+    // Set viewport and take screenshot
+    //await page.setViewport({ width: 1920, height: 1080 });
+
+    const totalFrames = 50;
+    const frameDelay = 100; // ms between frames (10 fps)
+
+    //const startedValue = await page.evaluate(() => window.renderStarted);
+    //console.log('window.renderStarted =', startedValue);
+
+    await page.waitForFunction(() => window.renderStarted === true, {
+      timeout: 15000 // optional: wait max 15 seconds
+    });
+
+    const screenshotBuffers = [];
+    for (let i = 0; i < totalFrames; i++) {
+      const screenshotBuffer = await page.screenshot({ fullPage: true });
+      screenshotBuffers.push(screenshotBuffer);
+      //console.log(`Captured frame ${i + 1}/${totalFrames}`);
+      await new Promise(resolve => setTimeout(resolve, frameDelay));  
+    }
+    await sharp(screenshotBuffers, { join: { animated: true }})
+    .toFile(path.join(imagesDir, `${model_name}.webp`), (err, info) => {
+      if (err) {
+        console.error('Error saving image:', err);
+      } else {
+        console.log('Screenshot saved:');
+      }
+    });
+  }
+
+  console.log('Screenshot saved to output.png');
+
+  await browser.close();
+})();
