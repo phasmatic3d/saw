2165269e04d85283baea96562d017482c4f9c8e2
agkar <a.gkaravelis@gmail.com>
2025-07-22 14:29:15 +0300

    update paths

--- src/lib/paths.tsx
+++ src/lib/paths.tsx
@@ -1,3 +1,4 @@
 
-export const basePath = process.env.NODE_ENV === 'production' ? '/glTF-Render-Fidelity' : '';
-export const baseUrl = "https://github.khronos.org/glTF-Render-Fidelity/";
+export const basePath = process.env.NODE_ENV === 'production' ? '/saw' : '';
+//export const baseUrl = "https://github.khronos.org/glTF-Render-Fidelity/";
+export const baseUrl = "https://phasmatic3d.github.io/saw/";
